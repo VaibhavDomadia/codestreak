31e5f2a7e06105b76001edac4d0265b139463fcc
Vaibhav Domadia <vjdomadia@gmail.com>
2021-03-29 14:45:49 +0530

    Add CORS and body parser

--- src/UserService/app.js
+++ src/UserService/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const userRoutes = require('./routes/user');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 app.use('/user', userRoutes);
 app.use('/admin', adminRoutes);
