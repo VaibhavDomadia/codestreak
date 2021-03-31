7820a6576653d56775b4af30bb65c983fb6d3f8f
Vaibhav Domadia <vjdomadia@gmail.com>
2021-03-31 18:39:29 +0530

    Add authentication check controller

---
+++ src/ContestService/controllers/auth.js
@@ -0,0 +1,34 @@
+const jsonWebToken = require('jsonwebtoken');
+
+/**
+ * Controller to validate token and check if the user is admin or not
+ */
+exports.isAuthenticated = (req, res, next) => {
+    try {
+        const token = req.get('Authorization');
+        if(!token) {
+            const error = new Error('Not Authenticated!');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const decodedToken = jsonWebToken.verify(token, 'secretKey');
+        if(!decodedToken) {
+            const error = new Error('Not Authenticated!');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        req.userID = decodedToken.userID;
+        req.isAdmin = decodedToken.isAdmin;
+        if(!req.isAdmin) {
+            const error = new Error('Not Authorized!');
+            error.statusCode = 403;
+            throw error;
+        }
+        next();
+    }
+    catch(error) {
+        next(error);
+    }
+}
