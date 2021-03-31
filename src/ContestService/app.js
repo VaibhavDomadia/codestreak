ee0094c11d0d8a9e1bf047e31a118d16a54a3d13
Vaibhav Domadia <vjdomadia@gmail.com>
2021-03-31 17:07:42 +0530

    Add CORS and body parser

--- src/ContestService/app.js
+++ src/ContestService/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const contestRoutes = require('./routes/contest');
 
 const app = express();
+
+/**
+ * To parse JSON
+ */
+app.use(bodyParser.json());
+
+/**
+ * To allow Cross Origin Resource Sharing (CORS)
+ */
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 app.use(contestRoutes);
 
