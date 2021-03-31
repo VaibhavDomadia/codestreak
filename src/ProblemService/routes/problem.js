dee0ec3b27fc88013d4886e5fe0280f9d0f004fb
Vaibhav Domadia <vjdomadia@gmail.com>
2021-03-31 12:27:43 +0530

    Add documentation comments for rest endpoints

--- src/ProblemService/routes/problem.js
+++ src/ProblemService/routes/problem.js
@@ -4,16 +4,34 @@ const problemController = require('../controllers/problem');
 
 const router = express.Router();
 
+/**
+ * REST Endpoint: GET /problems
+ */
 router.get('/problems', problemController.getProblems);
 
+/**
+ * REST Endpoint: GET /problem/:problemID
+ */
 router.get('/problem/:problemID', problemController.getProblem);
 
+/**
+ * REST Endpoint: POST /problem
+ */
 router.post('/problem', problemController.addProblem);
 
+/**
+ * REST Endpoint: GET /problem/:problemID/hiddencases
+ */
 router.get('/problem/:problemID/hiddencases', problemController.getHiddenCases);
 
+/**
+ * REST Endpoint: DELETE /problem/:problemID
+ */
 router.delete('/problem/:problemID', problemController.deleteProblem);
 
+/**
+ * REST Endpoint: PUT /problem/:problemID
+ */
 router.put('/problem/:problemID', problemController.updateProblem);
 
 module.exports = router;
