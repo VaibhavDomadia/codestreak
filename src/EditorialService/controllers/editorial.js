e16e16b8da332775beea287e75aad2a4ae6d331f
Vaibhav Domadia <vjdomadia@gmail.com>
2021-04-01 13:02:26 +0530

    Add controller to fetch all editorials of a problem

--- src/EditorialService/controllers/editorial.js
+++ src/EditorialService/controllers/editorial.js
@@ -3,23 +3,42 @@ const Editorial = require('../models/editorial');
 /**
  * Controller to fetch a editorial
  */
- exports.getEditorial = async (req, res, next) => {
+exports.getEditorial = async (req, res, next) => {
     const editorialID = req.params.editorialID;
 
     try {
         const editorial = await Editorial.findById(editorialID);
-        if(!editorial) {
+        if (!editorial) {
             throw new Error();
         }
 
-        res.status(200).json({editorial});
+        res.status(200).json({ editorial });
     }
-    catch(error) {
+    catch (error) {
         error.message = "Editorial doesn't exists";
         error.statusCode = 404;
         next(error);
     }
 }
+
+/**
+ * Controller to fetch all editorials of a problem
+ */
+exports.getProblemEditorials = async (req, res, next) => {
+    const problemID = req.params.problemID;
+
+    try {
+        const editorials = await Editorial.find({ problemID });
+
+        res.status(200).json({ editorials });
+    }
+    catch (error) {
+        error.message = "Problem doesn't exists";
+        error.statusCode = 404;
+        next(error);
+    }
+}
+
 
 /**
  * Controller to create a Editorial
