138794b1fd1e1d50a68c8c6d2eeaa2acd91070ad
Vaibhav Domadia <vjdomadia@gmail.com>
2021-04-01 15:02:42 +0530

    Add CORS and body parser

--- src/ProblemProposalService/app.js
+++ src/ProblemProposalService/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const proposalRoutes = require('./routes/proposal');
 
 const app = express();
+
+/**
+ * To parse JSON
+ */
+app.use(bodyParser.json());
+
+/**
+ * To Allow Cross Origin Resource Sharing (CORS)
+ */
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 /**
  * To Set Routes
