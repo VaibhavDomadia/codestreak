df90a3fda446516af26faf9d187f38029c71bdd2
Vaibhav Domadia <vjdomadia@gmail.com>
2021-04-27 16:27:41 +0530

    Add CORS

--- src/APIGateway/app.js
+++ src/APIGateway/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * To Allow Cross Origin Resource Sharing (CORS)
+ */
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.get('/', (req, res, next) => {
     console.log('Listening....');
 })
