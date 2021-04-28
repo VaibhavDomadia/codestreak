758cea02e7fc41f7bc3165aa58993953afcbf90d
Vaibhav Domadia <vjdomadia@gmail.com>
2021-04-28 11:26:34 +0530

    Add Navigation Bar

--- src/APIGateway/userfrontend/src/App.js
+++ src/APIGateway/userfrontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import NavigationBar from './Components/NavigationBar/NavigationBar';
 
 const App = (props) => {
     return (
         <div>
             <Header />
+            <NavigationBar />
         </div>
     )
 }
---
+++ src/APIGateway/userfrontend/src/Components/NavigationBar/NavigationBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './NavigationBar.css';
+import { NavLink } from 'react-router-dom';
+
+const NavigationBar = (props) => {
+    return (
+        <div className='NavigationBar-Container'>
+            <NavLink to='/' exact className='NavigationBar-Tile' activeClassName='NavigationBar-Tile-Active'>
+                Home
+            </NavLink>
+            <NavLink to='/contest' className='NavigationBar-Tile' activeClassName='NavigationBar-Tile-Active'>
+                Contests
+            </NavLink>
+            <NavLink to='/problem' className='NavigationBar-Tile' activeClassName='NavigationBar-Tile-Active'>
+                Problems
+            </NavLink>
+        </div>
+    )
+}
+
+export default NavigationBar;
