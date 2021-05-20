9454fcb32cb4578b6873b940e99cc8d012bfbc6f
Vaibhav Domadia <vjdomadia@gmail.com>
2021-05-21 00:51:03 +0530

    Add Navigation Bar

--- src/APIGateway/adminfrontend/src/App.js
+++ src/APIGateway/adminfrontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
+import NavigationBar from './Components/NavigationBar/NavigationBar';
 
 const App = (props) => {
     const [user, setUser] = useState(null);
@@ -8,6 +9,7 @@ const App = (props) => {
     return (
         <div className='App'>
             <Header user={user} setUser={setUser}/>
+            <NavigationBar />
         </div>
     )
 }
