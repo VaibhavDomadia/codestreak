61a90f86d76d81c2690d04b4ad2c432901989060
Vaibhav Domadia <vjdomadia@gmail.com>
2021-05-21 12:07:45 +0530

    Add Contest List Page

--- src/APIGateway/adminfrontend/src/App.js
+++ src/APIGateway/adminfrontend/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router';
 import './App.css';
 import Header from './Components/Header/Header';
 import NavigationBar from './Components/NavigationBar/NavigationBar';
+import ContestList from './Pages/ContestList/ContestList';
 import Error403 from './Pages/Error/Error403/Error403';
 import Error404 from './Pages/Error/Error404/Error404';
 import Error500 from './Pages/Error/Error500/Error500';
@@ -35,6 +36,7 @@ const App = (props) => {
                 <Route path='/login'>
                     <Login user={user} setUser={setUser}/>
                 </Route>
+                <Route path='/contest' exact component={ContestList}/>
                 <Route path='/403' exact component={Error403}/>
                 <Route path='/500' exact component={Error500}/>
                 <Route path='*' exact component={Error404}/>
